aae47b2a03eecdfc70d430a8497703dc3a0fa05f
Leandro <leandrobcrv@gmail.com>
2023-03-17 20:04:00 -0300

    Requisito 05

---
+++ challenges/desafio5.sql
@@ -0,0 +1,9 @@
+SELECT
+    m.music_name AS cancao, 
+	COUNT(hs.music_id) AS reproducoes
+FROM musics AS m
+INNER JOIN history AS hs ON m.music_id = hs.music_id
+
+GROUP BY hs.music_id
+ORDER BY reproducoes DESC, cancao 
+LIMIT 2;
